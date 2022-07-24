310b183d51ed275e61ece9e724eff88c6a5df723
seufster71 <seufster@gmail.com>
2022-07-24 16:07:23 -0400

    migrate

---
+++ acquaintances/acquaintances-view.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Table from '../../coreView/common/table';
+
+export default function AcquaintancesView({acquaintances}) {
+
+  let columns = [];
+  if (acquaintances.prefLabels != null && acquaintances.prefLabels.SOCIAL_ACQUAINTANCES_TABLE != null) {
+    columns = acquaintances.prefLabels.SOCIAL_ACQUAINTANCES_TABLE;
+  }
+  return (
+    <Table items={acquaintances.items} columns={columns} />
+  );
+}
+
+AcquaintancesView.propTypes = {
+  acquaintances: PropTypes.object
+};
